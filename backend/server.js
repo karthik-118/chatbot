7a50ff60a5879a81366e327bc70002b0ef969472
Ansh <ansh03012004@gmail.com>
2024-04-04 04:22:45 +0530

    permissions issue

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,6 @@ connectDB();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is Running");
-});
-
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
@@ -28,6 +24,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is Running");
   });
 }
 
